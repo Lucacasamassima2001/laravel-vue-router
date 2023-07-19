b182e20a63efc667381f542b47ebd737812f0715
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-07-19 18:12:19 +0200

    complete table seeder

--- database/seeders/DrinksTableSeeder.php
+++ database/seeders/DrinksTableSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Drink;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DrinksTableSeeder extends Seeder
 {
@@ -14,6 +15,8 @@ class DrinksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (config('drinks') as $drink) {
+            Drink::create($drink);
+        }
     }
 }
